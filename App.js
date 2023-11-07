c12ec5d7c7c153f8a023778a16b70e8024de7054
jay nam <woalskdl@gmail.com>
2023-11-07 23:05:23 +0900

    갤러리 UI 및 사진 추가, 삭제

--- App.js
+++ App.js
@@ -1,12 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, Dimensions, FlatList, Image, Platform, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useGallery } from './src/use-gallery';
+
+const width = Dimensions.get('screen').width;
+const columnSize = width / 3;
 
 export default function App() {
+
+  const {
+    images,
+    imagesWithAddButton,
+    pickImage,
+    deleteImage
+  } = useGallery();
+
+  const onPressOpenGallery = pickImage;
+  const onLongPressImage = (imageId) => deleteImage(imageId);
+
+  const renderItem = ({item : {id, uri}, idx}) => {
+    
+    if (id === -1) {
+      return (
+        <TouchableOpacity 
+          onPress={onPressOpenGallery}
+          style={{
+            width: columnSize, 
+            height: columnSize, 
+            backgroundColor: 'lightgrey',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
+          <Text style={{ fontWeight: '100', fontSize: 45}}>+</Text>
+        </TouchableOpacity>
+      )
+    }
+
+    return (
+      <TouchableOpacity onLongPress={() => onLongPressImage(id)}>
+        <Image source={{ uri }} style={{ width: columnSize, height: columnSize}}/>
+      </TouchableOpacity>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={imagesWithAddButton}
+        renderItem={renderItem}
+        numColumns={3}
+      />
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +57,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    marginTop: Platform.OS === 'android' ? 30 : 0
+  }
 });
